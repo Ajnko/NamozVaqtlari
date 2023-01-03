b2b7d7d8659749b667994833fa9cf0f7224f7f6c
Ajnko <abdulbosijalilov28@gmail.com>
2023-01-03 16:17:39 +0500

    Initial Commit

---
+++ NamozVaqtlari_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamozVaqtlari_API
+//
+//  Created by Abdulbosid Jalilov on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
